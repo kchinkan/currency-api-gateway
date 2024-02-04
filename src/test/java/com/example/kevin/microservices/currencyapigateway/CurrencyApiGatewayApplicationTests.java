3f88507704ab3bbebba48e325aa863f4888d34a6
Kevin Chinkan <kchinkan@gmail.com>
2024-02-04 14:44:26 -0500

    initialize spring project

---
+++ src/test/java/com/example/kevin/microservices/currencyapigateway/CurrencyApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kevin.microservices.currencyapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
